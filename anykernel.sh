1b96b089740fc4c6ee4b6c0a076325a7e1aab65e
A. YulianT <85273605+TeraaBytee@users.noreply.github.com>
2021-10-16 21:37:31 +0700

    anykernel.sh: Update device for begonia and begoniain
    Singed-off-by: TeraaBytee <terabyte3766@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -10,17 +10,14 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=begonia
+device.name2=begoniain
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
